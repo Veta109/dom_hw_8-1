97e00cc6c716c1779c96751f949159eedd4d4fa0
Veta <tsoyveta2008@gmail.com>
2025-10-23 23:33:18 +0500

    all done

---
+++ script.js
@@ -0,0 +1,4 @@
+let data = ["Иван", "Иванов", "Отдел разработки", "Программист", 2000];
+
+let [name, surname, department, position, salary] = data;
+console.log(name, surname, department, position, salary); 
