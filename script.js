53fb6d295af72fb10673f24d02db66c0b150a1d0
Veta <tsoyveta2008@gmail.com>
2025-10-25 16:25:12 +0500

    done

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-let data = ["Иван", "Иванов", "Отдел разработки", "Программист", 2000];
+//-------------------------------------задание 1---------------------------------------------
+// let data = ["Иван", "Иванов", "Отдел разработки", "Программист", 2000];
 
-let [name, surname, department, position, salary] = data;
-console.log(name, surname, department, position, salary); 
+// let [name, surname, department, position, salary] = data;
+// console.log(name, surname, department, position, salary); 
+
+
+
+//-------------------------------------задание 2---------------------------------------------
+// const data = ["Иван", "Иванов", "Отдел разработки", "Программист", 2000];
+
+// let [name, surname, info ] = data;
+// console.log(name, surname, info); 
+
+
+
+//-------------------------------------задание 3---------------------------------------------
+// const data = ["Иван", "Иванов", "Отдел разработки", undefined, 2000];
+
+// let [name, surname, department, position = "Junior"] = data;
+// console.log(name, surname, department, position);
+
+
+
+//-------------------------------------задание 4---------------------------------------------
+const data = {
+    color: "red",
+    width: 400,
+    height: 500,
+};
+
+let {color, width, height} = data;
+console.log(color, width, height);
